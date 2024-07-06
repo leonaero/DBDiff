7a87d8bb9e035069bff93996777e98478d3fc9c3
Adam Grzegorczyk <agrzegorczyk@leonsoftware.com>
2024-07-06 22:13:19 +0200

    little improvement for run-image.sh

--- run-image.sh
+++ run-image.sh
@@ -4,6 +4,6 @@ set -eu
 
 cd "$(dirname "${0}")"
 
+CMD="${1:-bash}"
 . image
-docker run -it --rm --network host -v "$(pwd -P)":/app -u "$(id -u):$(id -g)" "${IMAGE}" bash
-
+docker run -it --rm --network host -v "$(pwd -P)":/app -u "$(id -u):$(id -g)" "${IMAGE}" "${CMD}"
